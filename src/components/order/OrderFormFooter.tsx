f30975cd58b4b097e175ea7c607b4b24cfc63e1f
Darij <darijusbrizgys@gmail.com>
2026-07-28 16:13:13 +0100

    make order form footer scroll on tablet instead of fixed

--- src/components/order/OrderFormFooter.tsx
+++ src/components/order/OrderFormFooter.tsx
@@ -57,7 +57,7 @@ export function OrderFormFooter({
 
   return (
     <div
-      className="relative sm:fixed sm:bottom-0 sm:left-0 sm:right-0 bg-white border-t border-gray-200 p-4 shadow-lg sm:z-50"
+      className="relative lg:fixed lg:bottom-0 lg:left-0 lg:right-0 bg-white border-t border-gray-200 p-4 shadow-lg lg:z-50"
       style={{ paddingBottom: 'calc(1rem + env(safe-area-inset-bottom))' }}
     >
       <div className="max-w-4xl mx-auto flex flex-col sm:flex-row items-center justify-between gap-4">
---
+++ tmp_git_push5.ps1
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "make order form footer scroll on tablet instead of fixed"
+git push origin main
